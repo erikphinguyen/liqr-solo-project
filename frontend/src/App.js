bc79eff187635eecf1e612e4e1b9567ead4235db
erikphinguyen <erikphinguyen@gmail.com>
2022-06-25 21:42:14 -0700

    questionable LoginFormPage error

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,15 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+// import LoginFormPage from './components/LoginFormPage';
+// -----> error: Module not found: Error: Can't resolve './components/LoginFormPage' in '/home/erikphinguyen/appacademy/liqr-solo-project/frontend/src'
 function App() {
   return (
-    <h1>Hello from App</h1>
+    <Switch>
+      <h1>Hello World!</h1>
+      <Route path="/login">
+        {/* <LoginFormPage /> */}
+      </Route>
+    </Switch>
   );
 }
 
